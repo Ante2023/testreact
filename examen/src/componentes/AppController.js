54d97503c15664b4982760d71c4f3e27fbaed156
Ante2023 <appsoftest@gmail.com>
2023-08-05 12:31:25 +0200

    paso lista

--- examen/src/componentes/AppController.js
+++ examen/src/componentes/AppController.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import TaskList from "./TaskList";
+
 
 const AppController = () => {
-  return (
-	<div>AppController</div>
-  )
-}
+  const [nombre, setombre]=useState([])
 
-export default AppController
+  useEffect(() => {
+    fetch(`http://10.212.160.134:3000/ex1`)
+      .then((resp) => resp.json())
+      .then((data) => setombre(data));
+      // console.log(nombre);
+  }, []);
+
+  return <div><TaskList data ={nombre}/></div>;
+};
+
+export default AppController;
