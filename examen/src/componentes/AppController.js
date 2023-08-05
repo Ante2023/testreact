cddd233d5d64a2539e1094246a75b48b62462c5f
Ante2023 <appsoftest@gmail.com>
2023-08-05 13:06:50 +0200

    cambiando funcion que recibe ID

--- examen/src/componentes/AppController.js
+++ examen/src/componentes/AppController.js
@@ -4,26 +4,25 @@ import TaskItem from "./TaskItem";
 
 const AppController = () => {
   const [taskListItems, setTaskListItems] = useState([]);
-  const[taskClicked,setTaskClicked]= useState("")
+  const [taskClicked, setTaskClicked] = useState("");
 
   useEffect(() => {
     fetch(`http://10.212.160.134:3000/ex1`)
       .then((resp) => resp.json())
-      .then((data) => setombre(data));
+      .then((data) => setTaskListItems(data));
     // console.log(nombre);
   }, []);
 
-  const onTaskClickedHandled = (id)=>{
-    setTaskClicked(taskListItems.filter(item=>item.id === id)[0])
-  }
+  const onTaskClickedHandled = (id) => {
+    setTaskClicked(taskListItems.filter((item) => item.id === id)[0]);
+  };
 
   return (
     <div>
-      <TaskList data={taskListItems}  onTaskClicked = {onTaskClickedHandled}/>
-      {
-        taskClicked &&
-      <TaskItem task={taskListItems} />
-      }
+      {taskListItems && (
+        <TaskList data={taskListItems} onTaskClicked={onTaskClickedHandled} />
+      )}
+      {taskClicked && <TaskItem task={taskListItems} />}
     </div>
   );
 };
